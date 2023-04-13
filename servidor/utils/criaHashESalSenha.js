be93e4de42c6897d569b4bebb44a6ed37bede5bd
AshileySabah <ashileysabrina2002@gmail.com>
2023-04-13 19:26:27 -0300

    feat: cadastro, login, autenticacao e gerar jwt

--- servidor/utils/criaHashESalSenha.js
+++ servidor/utils/criaHashESalSenha.js
@@ -2,7 +2,9 @@ import { randomBytes, scryptSync } from "crypto";
 
 function criaHashESalSenha(senhaDigitada) {
   const salSenha = randomBytes(16).toString("hex");
+
   const hashSenha = scryptSync(senhaDigitada, salSenha, 64).toString("hex");
+
   return { salSenha, hashSenha };
 }
 
